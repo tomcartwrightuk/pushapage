e826b02b255e13ba07d7c77e6adc3af5d169f687
Tom Cartwright <tecartwright@gmail.com>
2012-05-08 18:28:30 +0100

    Added sqlite to development environment only

--- Gemfile
+++ Gemfile
@@ -5,7 +5,9 @@ gem 'rails', '3.0.9'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+group :development do
+  gem 'sqlite3'
+end
 gem 'devise'
 gem 'rspec-rails', '2.6.1'
 gem 'factory_girl'
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  before(:each) do
+    @user = Factory(:user)
+    sign_in @user
+  end
+   
+  describe "insta_check" do
+    it "should respond using ajax if instapaper account present" do
+        lambda do
+          xhr :get, :insta_check
+          response should be_true
+        end
+    end
+  end
+
+end
