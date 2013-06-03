81773dad5b03255755836c562520afb9deb27e11
Tom Cartwright <tecartwright@gmail.com>
2013-06-03 09:25:19 +0100

    Move old application JS to custom.js

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,33 @@
-//= require jquery
-//= require jquery_ujs
-//= require_self
-//= require_tree .
+$(function() {
+  $('#sign_in_button').click(function() {
+    $('#sign_in').fadeIn(300);
+  });
+  $('#settings_button').click(function() {
+    $("#submenu_item").toggle();
+  });
 
-/* rest of file omitted */
+  $('body').click(function(event) {
+    if (!$(event.target).closest('#settings_button').length) {
+    $('#submenu_item').hide();
+    };
+  });
 
+  var window_height = $(window).height();
+  $('.container').css('min-height', 0.8*window_height);
+
+  $('#user_new').validate({
+    rules: {
+      "user[email]": {required: true, email: true, remote:"/users/check_email" },
+      "user[password]": {
+        required:true,
+        rangelength:[6,16]
+      },
+      "user[password_confirmation]": {
+        equalTo: '#user_password'
+      }
+    }, //end rules
+    success: function(label) {
+       label.html("<img src='/images/tick.png'>").addClass("checked");
+    }
+  }); // end validate(),
+});
